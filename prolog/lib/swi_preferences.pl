aabcc5dc50e262f4e8a5622b2a92340f24ba3dd8
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2011-05-30 16:57:49 +0200

    CLEANUP: Old confit stuff.

--- prolog/lib/swi_preferences.pl
+++ prolog/lib/swi_preferences.pl
@@ -3,9 +3,10 @@
     Part of XPCE --- The SWI-Prolog GUI toolkit
 
     Author:        Jan Wielemaker and Anjo Anjewierden
-    E-mail:        jan@swi.psy.uva.nl
+    E-mail:        J.Wielemaker@cs.vu.nl
     WWW:           http://www.swi.psy.uva.nl/projects/xpce/
-    Copyright (C): 1985-2002, University of Amsterdam
+    Copyright (C): 1985-2011, University of Amsterdam
+			      VU University Amsterdam
 
     This program is free software; you can redistribute it and/or
     modify it under the terms of the GNU General Public License
@@ -35,10 +36,13 @@
 :- use_module(library(pce)).
 :- use_module(library(pce_tick_box)).
 
-:- pce_help_file(plprefs, pce_help('plprefs.hlp')).
+%%	prolog_edit_preferences(+What) is det.
+%
+%	Edit the specified user preference file.  What is one of
+%
+%	    * xpce
+%	    * prolog
 
-prolog_edit_preferences(stack_sizes) :-
-	send(new(prolog_preferences), open_centered).
 prolog_edit_preferences(What) :-
 	locate_preferences(What, File),
 	auto_call(start_emacs),
@@ -57,9 +61,6 @@ prolog_edit_preferences(What) :-
 	send(@emacs, goto_source_location, File).
 
 locate_preferences(xpce, File) :-
-	get(@pce, home, Home),
-	get(string('%s/Defaults', Home), value, File).
-locate_preferences(xpce_user, File) :-
 	ensure_xpce_config_dir(Dir),
 	get(string('%s/Defaults', Dir), value, File).
 locate_preferences(prolog, File) :-
@@ -89,7 +90,7 @@ default_preferences(prolog, File) :-
 			   [ access(read),
 			     file_errors(fail)
 			   ], File), !.
-default_preferences(xpce_user, File) :-
+default_preferences(xpce, File) :-
 	absolute_file_name(pce('Defaults.user'),
 			   [ access(read),
 			     file_errors(fail)
@@ -111,93 +112,3 @@ ensure_xpce_config_dir(Dir) :-
 
 copy_file(From, To) :-
 	send(file(To), copy, From).
-
-
-		 /*******************************
-		 *	    STACK SIZES		*
-		 *******************************/
-
-stack(local,    locallimit,    localSize).
-stack(global,   globallimit,   globalSize).
-stack(trail,    traillimit,    trailSize).
-stack(argument, argumentlimit, argumentSize).
-
-:- pce_begin_class(prolog_preferences, dialog,
-		   "Edit the default preferences").
-
-initialise(D) :->
-	send_super(D, initialise, 'SWI-Prolog registry preferences'),
-	forall(stack(Name, _, _),
-	       send(D, append, prolog_stack_size_item(Name))),
-	send(D, append, button(cancel)),
-	send(D, append, button(apply)),
-	send(D, append, button(help)),
-	send(D, default_button, apply).
-
-cancel(D) :->
-	send(D, destroy).
-
-help(_D) :->
-	"Show window with help-text"::
-	send(@helper, give_help, plprefs, main).
-
-apply(D) :->
-	(   get(D?graphicals, find,
-		and(message(@arg1, has_get_method, modified),
-		    @arg1?modified == @on),
-		_)
-	->  send_super(D, apply),
-	    send(@display, inform,
-		 'Saved new stack limits in the Windows registry\n\
-		  These settings become active the next time you start Prolog')
-	;   true
-	),
-	send(D, destroy).
-
-:- pce_end_class(prolog_preferences).
-
-
-:- pce_begin_class(prolog_stack_size_item, label_box).
-
-initialise(I, Stack:name) :->
-	send_super(I, initialise, Stack),
-	stack(Stack, Key, _),
-	statistics(Key, Current),
-	(   Current =:= 128*1024*1024
-	->  DefLimit = @off,
-	    Def = 0
-	;   DefLimit = @on,
-	    Def is round(Current/(1024*1024))
-	),
-	send(I, append, tick_box(limit_to, DefLimit)),
-	send(I, append,
-	     int_item('Mb', Def, @default, 0, 128), right).
-
-limit_to(I, Limit:bool) :->
-	"Trap change of limit"::
-	get(I, member, 'Mb', IntItem),
-	send(IntItem, active, Limit).
-
-apply(I, Always:[bool]) :->
-	"Save to the registry"::
-	(   (   Always == @on
-	    ;   get(I, modified, @on)
-	    )
-	->  get(I, name, Stack),
-	    get(I, member, limit_to, LimitItem),
-	    (   get(LimitItem, selection, @on)
-	    ->  get(I, member, 'Mb', IntItem),
-		(   get(IntItem, selection, Mb)
-		->  Kb is Mb * 1024
-		;   send(I, report, error, 'Limit out of range (1..128 MB)'),
-		    fail
-		)
-	    ;   Kb = 0
-	    ),
-	    stack(Stack, _, KeyName),
-	    registry_set_key(current_user/'Software'/'SWI'/'Prolog',
-			     KeyName, Kb)
-	;   true
-	).
-
-:- pce_end_class.
