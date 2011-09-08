2daafdabe5ea7c84647110aae25a436cb1c86fab
Keri Harris <keri.harris@securitease.com>
2011-09-09 08:47:15 +1200

    Avoid redefinition of INT32 on MinGW

--- src/msw/msjpeg.c
+++ src/msw/msjpeg.c
@@ -31,7 +31,7 @@ by msimage.c, which in  turn  implements   the  OS  specific  version of
 gra/image.c implementing class image.
 - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
-#ifdef __CYGWIN__
+#if defined(__CYGWIN__) || defined(__MINGW32__)
 #define XMD_H
 #endif
 
