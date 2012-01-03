5757b7d1da86653f8deab233d9d0bab004f64e0d
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2012-01-03 16:54:02 +0100

    Avoid using cat (might also work on Windows)

--- prolog/lib/emacs/fundamental_mode.pl
+++ prolog/lib/emacs/fundamental_mode.pl
@@ -130,7 +130,7 @@ class_variable(grep_command, string,
 	       ],
 	       "Command of M-x grep").
 class_variable(git_grep_command, string,
-	       'git grep -n %s | cat',
+	       'git --no-pager grep -n %s',
 	       "Command of M-x git-grep").
 class_variable(shell_command, chain*,
 	       when(@pce?window_system == windows,
