e7a90f5e1085d73ef853c0af8ae32506a07f847d
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2012-04-09 16:53:51 +0200

    Change default for font.scale to 1.0 on X11

--- src/gra/font.c
+++ src/gra/font.c
@@ -442,7 +442,7 @@ static getdecl get_font[] =
 /* Resources */
 
 static classvardecl rc_font[] =
-{ RC(NAME_scale, "real", "1.4",
+{ RC(NAME_scale, "real", UXWIN("1.0", "1.4"),
      "Multiplication factor for all fonts")
 };
 
