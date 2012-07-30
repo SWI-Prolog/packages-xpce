f58d0c648a0e159de8a4480538e78ac57a3e3799
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2012-07-30 09:46:16 +0200

    ISO: Use \c instead of \<newline>

--- prolog/lib/swi_ide.pl
+++ prolog/lib/swi_ide.pl
@@ -1,11 +1,9 @@
-/*  $Id$
-
-    Part of XPCE --- The SWI-Prolog GUI toolkit
+/*  Part of XPCE --- The SWI-Prolog GUI toolkit
 
     Author:        Jan Wielemaker and Anjo Anjewierden
     E-mail:        J.Wielemaker@cs.vu.nl
     WWW:           http://www.swi-prolog.org
-    Copyright (C): 1985-2011, University of Amsterdam
+    Copyright (C): 1985-2012, University of Amsterdam
 			      VU University Amsterdam
 
     This program is free software; you can redistribute it and/or
@@ -166,7 +164,7 @@ thread_monitor(IDE) :->
 	    ),
 	    send(Monitor, open)
 	;   send(@display, report, error,
-		 'This version of SWI-Prolog is not built\n\
+		 'This version of SWI-Prolog is not built\n\c
 		  with thread-support')
 	).
 
