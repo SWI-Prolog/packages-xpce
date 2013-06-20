2cc4304d180e416bb39527c3bdacf0e6ad4a0980
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2013-06-20 14:45:47 +0200

    MODIFIED: JavaScript mode: Alignment of name: function() {

--- prolog/lib/emacs/java_script_mode.pl
+++ prolog/lib/emacs/java_script_mode.pl
@@ -42,7 +42,10 @@ back_skip_if_etc(E, Pos:int, Start:int) :<-
 	"Find indent for stuff before {"::
 	get_super(E, back_skip_if_etc, Pos, Start0),
 	get(E, scan, Pos, line, 0, start, SOL),
-	(   get(E, looking_at, 'function\\s*', Start0, SOL, Len)
+	(   get(E, looking_at, '(\\w+:\\s*)?function\\s*', Start0, SOL, Len)
+	->  Start is Start0 - Len
+	;   get(E, looking_at, function, Start0, _),
+	    get(E, looking_at, '\\w+:\\s*', Start0, SOL, Len)
 	->  Start is Start0 - Len
 	;   Start = Start0
 	).
