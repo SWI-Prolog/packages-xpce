a331b785590f7be08314466f0ad3a04166ba4186
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2018-09-21 14:27:15 +0200

    Deal with failing detection of jpeglib.h due to dependency on size_t.

--- cmake/XPCEConfig.cmake
+++ cmake/XPCEConfig.cmake
@@ -5,6 +5,7 @@ include(TestBSDSignals)
 string(TOLOWER ${CMAKE_HOST_SYSTEM_NAME} OS)
 
 set(CMAKE_REQUIRED_LIBRARIES ${CMAKE_REQUIRED_LIBRARIES} ${GUI_LIBRARIES})
+set(CMAKE_REQUIRED_INCLUDES ${CMAKE_REQUIRED_INCLUDES} ${GUI_INCLUDE_DIRS})
 
 check_type_size("int" SIZEOF_INT)
 check_type_size("long" SIZEOF_LONG)
@@ -15,7 +16,7 @@ AC_CHECK_HEADERS(unistd.h string.h memory.h time.h sys/time.h sys/file.h pwd.h)
 AC_CHECK_HEADERS(sys/select.h sys/param.h malloc.h sys/resource.h stropts.h)
 AC_CHECK_HEADERS(frame.h sys/timeb.h sys/times.h siginfo.h bstring.h)
 AC_CHECK_HEADERS(sys/socketvar.h conio.h sys/access.h termios.h termio.h)
-AC_CHECK_HEADERS(sched.h crt_externs.h poll.h jerror.h jpeglib.h)
+AC_CHECK_HEADERS(sched.h crt_externs.h poll.h jerror.h)
 AC_CHECK_HEADERS(X11/extensions/Xinerama.h)
 
 AC_CHECK_FUNCS(vsscanf on_exit memmove select popen strerror timelocal)
