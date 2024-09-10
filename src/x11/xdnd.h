5487ca2265f40d21d04c6c7676cb5160eba23004
Jan Wielemaker <jan@swi-prolog.org>
2024-09-10 21:26:27 +0200

    Fixed mixed LGPL/GPL claim for xdnd.h

--- src/x11/xdnd.h
+++ src/x11/xdnd.h
@@ -14,11 +14,6 @@
    You should have received a copy of the GNU Lesser General Public
    License along with this library; if not, write to the Free Software
    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
-
-   You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
-   02110-1301, USA
  */
 
 #ifndef _X_DND_H
