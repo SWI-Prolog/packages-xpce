4c536a6c5768ac814234fe7780e8385a8b8b1a70
Jan Wielemaker <jan@swi-prolog.org>
2025-05-24 19:02:52 +0200

    Comment

--- prolog/lib/terminal.pl
+++ prolog/lib/terminal.pl
@@ -1,17 +1,69 @@
+/*  Part of SWI-Prolog
+
+    Author:        Jan Wielemaker
+    E-mail:        jan@swi-prolog.org
+    WWW:           http://www.swi-prolog.org
+    Copyright (c)  2025, SWI-Prolog Solutions b.v.
+    All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions
+    are met:
+
+    1. Redistributions of source code must retain the above copyright
+       notice, this list of conditions and the following disclaimer.
+
+    2. Redistributions in binary form must reproduce the above copyright
+       notice, this list of conditions and the following disclaimer in
+       the documentation and/or other materials provided with the
+       distribution.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+    "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+    LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+    FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+    COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+    INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+    BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+    LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+    CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+    LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+    ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+    POSSIBILITY OF SUCH DAMAGE.
+*/
+
 :- module(terminal,
-          [ terminal/1
+          [ terminal/1,                 % +Title
+            terminal/0
           ]).
 :- use_module(library(pce)).
-:- use_module(library(threadutil)).
+:- use_module(library(threadutil), []).
 :- use_module(library(edit)).
 :- use_module(library(pce_util)).
 :- use_module(library(uri)).
 :- use_module(library(www_browser)).
 :- use_module(library(gensym)).
-:- use_module(library(time)).
 
-terminal(T) :-
-    send(new(T, terminal), open).
+/** <module> XPCE Embedded terminals
+
+This module implements embedded terminals   for XPCE. Embedded terminals
+replace `swipl-win`, both the native Win32 version and the Qt version.
+
+@tbd   The   current   version   leans   on    the   console   code   of
+library(thread_util). Eventually, this should be properly merged.
+@tbd Add a frame with menu bar and tabbed windows for the terminals. Or,
+    split horizontal/vertical, like terminator.
+*/
+
+%!  terminal is det.
+%!  terminal(+Title) is det.
+%
+%   Create a new terminal and open it.
+
+terminal :-
+    terminal(@default).
+terminal(Title) :-
+    send(new(terminal(Title)), open).
 
 :- dynamic
     current_prolog_terminal/2.          % Thread, Terminal
@@ -198,6 +250,6 @@ open(T) :->
     "Open the terminal and attach a Prolog thread to it"::
     send_super(T, open),
     get(T, member, terminal, TI),
-    connect(TI, "Test").
+    connect(TI, _Title).
 
 :- pce_end_class(terminal).
